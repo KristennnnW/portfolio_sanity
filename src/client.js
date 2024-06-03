e1352f7c5d8fda88490ada139396c0e1aabb17e5
KristennnnW <89870255+KristennnnW@users.noreply.github.com>
2024-06-03 16:34:28 -0500

    connect with sanity - post

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
-import { SanityClient } from "@sanity/client";
+import sanityClient from "@sanity/client";
 
 export default sanityClient({
     projectId: "iaftt32f",
-    dataset: "production"
-})
+    dataset: "production",
+});
