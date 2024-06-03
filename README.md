6a246b9a0609ca8e79924522535f203401822085
KristennnnW <89870255+KristennnnW@users.noreply.github.com>
2024-06-03 14:54:08 -0500

    added components & tailwind

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # portfolio
 This is a sandbox project for WWT cms testing.
+
+references:
+https://www.youtube.com/watch?v=NO7_jgzVgbc
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import { SanityClient } from "@sanity/client";
+
+export default sanityClient({
+    projectId: "iaftt32f",
+    dataset: "production"
+})
