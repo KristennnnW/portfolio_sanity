3a54e9501ec244b7417dd79045d43ad1703f3cc7
KristennnnW <89870255+KristennnnW@users.noreply.github.com>
2024-06-04 14:25:07 -0500

    cannot get project up in sanity

--- src/components/Post.js
+++ src/components/Post.js
@@ -20,13 +20,13 @@ export default function Post(){
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
 
                     {postData && postData.map((post, index) => (
-                        
+
                     <article>
                         <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                         <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400" key={index}>
                             <img src={post.mainImage.asset.url} alt={post.mainImage.alt} className="w-full h-full rounded-r object-cover absolute"/>
                             <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                                <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-blue-700 text-blue-100 bg-opacity-75 rounded">
+                                <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-blue-700 text-red-100 bg-opacity-75 rounded">
                                     {post.title}
                                 </h3>
                             </span>
